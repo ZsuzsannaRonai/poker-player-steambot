77b2c2fbce4587c1e45825c8035b99801ca42c16
Daniel Zsk <dzsukov@gmail.com>
2019-08-26 11:29:09 +0200

    my rank renamed

--- player.py
+++ player.py
@@ -5,15 +5,22 @@ class Player:
     def betRequest(self, game_state):
 
         players = game_state["players"]
-        my_hand = {}
+
+        my_cards = {}
         for player in players:
             if player["name"] == "SteamBot":
-                my_hand = player["hole_cards"]
+                my_cards = player["hole_cards"]
+
+        community_cards = {}
+
+
         my_ranks = []
-        for card in my_hand:
+        for card in my_cards:
             my_ranks.append(card["rank"])
         if my_ranks[0] == my_ranks[1]:
             return game_state["minimum_raise"]
+
+
         return 0
 
     def showdown(self, game_state):
