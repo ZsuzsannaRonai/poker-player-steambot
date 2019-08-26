0794186f9651bdbaea5c315c28b6ad57ca35410e
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-08-26 10:48:55 +0200

    getting my cards - just started

--- player.py
+++ player.py
@@ -3,6 +3,13 @@ class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
+        players = game_state["players"]
+        my_hand = {}
+        for player in players:
+            if player["name"] == "SteamBot":
+                my_hand = player["hole_cards"]
+
+
         return 0
 
     def showdown(self, game_state):
