256331d65b4b5628b3d13799944cd8c8a7a4f42e
Lux0n <gyuri1994@gmail.com>
2019-08-26 12:01:14 +0200

    frequency counter

--- player.py
+++ player.py
@@ -112,6 +112,13 @@ class Player:
             return game_state["minimum_raise"]
         return 0
 
+    def frequency_in_community(self, community_ranks):
+        # returns a dictionary with ranks as keys and duplication as value
+        frequency = {}
+        for rank in community_ranks:
+            frequency[rank] = frequency.get(rank, 0) + 1
+        return frequency
+
     def showdown(self, game_state):
         pass
 
